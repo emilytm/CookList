da2e681acc12662f459446a967f471c97856b7fc
emilytm <113457923+emilytm@users.noreply.github.com>
2023-01-26 10:23:42 -0500

    Working endtoend but need to refactor myRecipes

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import { parseResults, searchAll, searchMyRecipes, formatResults, renderResults,
 let searchArea = document.getElementById('search-wrapper')
 var recipesList = []
 
+/*document.addEventListener('DOMContentLoaded',() => {
+    if(window.location.href.indexOf('myrecipes') !== -1){
+        if(JSON.parse(localStorage.getItem('recipesList')).length > 0){
+            renderResults(JSON.parse(localStorage.getItem('recipesList')).filter(rec => rec.saved === true))
+        }
+    }
+})*/
+
 searchArea.addEventListener('submit', async function(e){
     e.preventDefault()
     let searchTerm = searchArea.elements[0].value
-    document.getElementById('message-display').classList.add('hidden')
     
     if(window.location.href.indexOf('myrecipes') !== -1) {
         searchMyRecipes(searchTerm,recipesList)
--- utils.js
+++ utils.js
@@ -4,15 +4,21 @@ export async function searchAll(searchTerm,recipesList){
     recipesList = formatResults(results)
     renderResults(recipesList)
     localStorage.setItem('recipesList',JSON.stringify(recipesList))
+    console.log(`in searchAll the recipesList after searching for ${searchTerm} is:`)
+    console.log(`${JSON.parse(localStorage.getItem('recipesList'))}`)
     return recipesList
 }
+
 export function searchMyRecipes(searchTerm){
     let recipesList = JSON.parse(localStorage.getItem('recipesList'))
     if(searchTerm){
         let filteredRecipes = recipesList.filter(rec => (rec.saved === true && rec.name.indexOf(searchTerm) !== -1))
         renderResults(filteredRecipes)
+        console.log(`in searchMyRecipes the recipesList after searching for ${searchTerm} is:`)
+        console.log(`${filteredRecipes.map(x => x.name)}`)
     }
 }
+
 export function toggleSave(uniqueUri){
     let recipesList = JSON.parse(localStorage.getItem('recipesList'))
     const recipeToToggle = recipesList.find(recipe => recipe.uniqueUri === uniqueUri)
@@ -32,17 +38,16 @@ export function toggleSave(uniqueUri){
     }
     recipesList[index].saved = !recipesList[index].saved
     localStorage.setItem('recipesList',JSON.stringify(recipesList))
+    console.log(`in toggleSave the recipesList after toggling save for ${recipeToToggle.name} is:`)
+    console.log(`${JSON.parse(localStorage.getItem('recipesList')).filter(item => item.saved)}`)
 }
 
 export async function search(searchString){
-
     let getResults = await fetchJson(`https://api.edamam.com/api/recipes/v2?type=any&beta=true&q=${searchString}&app_id=08159f45&app_key=%207cde058d95c392d413b4017227de3d3a`)
     return getResults.hits
-
 }
 
 export async function parseResults(searchTerm){
-
     let searchString = searchTerm.replaceAll(' ','%20')
     let matchResults = await search(searchString)
     let recipeResults = []
@@ -67,14 +72,11 @@ export async function parseResults(searchTerm){
         })
     }
     return recipeResults
-
 }
 
 export async function fetchJson(url){
-
     let result = await fetch(url)
     return result.json()
-
 }
 
 export function selectArray(recipeList) {
@@ -86,31 +88,48 @@ export function selectArray(recipeList) {
 }
 
 export function renderResults(recipeArray) {
-    let resultsHtml = ""
-    recipeArray = selectArray(recipeArray)
-    for ( let recipe of recipeArray ) {
-        let imageLink = recipe.image
-        let ingredients = []
-        recipe.ingredients.forEach(food => {
-            ingredients.push(food.text)
-        });
-        let ingredientString = ingredients.join(", ")
-        resultsHtml += `
-        <div class="result-item">
-            <img class="result-img" alt="recipe image for Tahini Shortbread Cookies" src="${imageLink}">
-            <p class="title first-row left-align">${recipe.name}</p>
-            <div class='list-toggle-btn' id='list-toggle-btn' data-recipe="${recipe.uniqueUri}">
-                <img class='list-toggle-icon' id='list-toggle-icon' src="/add.svg" alt="add/remove icon" data-recipe="${recipe.uniqueUri}">
-                <p class="list-toggle-text" id="list-toggle-text" data-recipe="${recipe.uniqueUri}">My Recipes</p>
-            </div>
-            <p class="source left-align"><a href="${recipe.link}">${recipe.source}</a></p>
-            <p class="tags left-align">${recipe.mealType}, ${recipe.dishType}</p>
-            <p class="cuisine">${recipe.cuisine}</p>
-            <p class="ingredient-preview left-align end-in-ellipse">${ingredientString}</p>
-        </div>        
-        `
+    if(recipeArray.length >= 1){  
+        document.getElementById('message-display').classList.add('hidden')
+        let resultsHtml = ""
+        //recipeArray = selectArray(recipeArray)
+        for ( let recipe of recipeArray ) {
+            let imageLink = recipe.image
+            let ingredients = []
+            recipe.ingredients.forEach(food => {
+                ingredients.push(food.text)
+            })
+            let interactionIcon
+            let interactionText
+            let interactionAlt
+            if (recipe.saved) {
+                interactionIcon = '/remove.svg'
+                interactionText = 'Remove'
+                interactionAlt = 'Remove icon for Remove from My Recipes button'
+            } else {
+                interactionIcon = '/add.svg'
+                interactionText = 'My Recipes'
+                interactionAlt = 'Add icon for Add to My Recipes button'
+            }
+            let ingredientString = ingredients.join(", ")
+            resultsHtml += `
+            <div class="result-item">
+                <img class="result-img" alt="recipe image for Tahini Shortbread Cookies" src="${imageLink}">
+                <p class="title first-row left-align">${recipe.name}</p>
+                <div class='list-toggle-btn' id='list-toggle-btn' data-recipe="${recipe.uniqueUri}">
+                    <img class='list-toggle-icon' id='list-toggle-icon' src="${interactionIcon}" alt="${interactionAlt}" data-recipe="${recipe.uniqueUri}">
+                    <p class="list-toggle-text" id="list-toggle-text" data-recipe="${recipe.uniqueUri}">${interactionText}</p>
+                </div>
+                <p class="source left-align"><a href="${recipe.link}">${recipe.source}</a></p>
+                <p class="tags left-align">${recipe.mealType}, ${recipe.dishType}</p>
+                <p class="cuisine">${recipe.cuisine}</p>
+                <p class="ingredient-preview left-align end-in-ellipse">${ingredientString}</p>
+            </div>        
+            `
+        }
+        document.getElementById('results-list').innerHTML = resultsHtml
+    } else {
+        renderNoResultState()
     }
-    document.getElementById('results-list').innerHTML = resultsHtml
 }
 
 export function formatResults(recipeResults){
@@ -121,7 +140,6 @@ export function formatResults(recipeResults){
         recipe.cuisine = recipe.cuisine.map(cuisine => titleCase(cuisine))
         recipe.dishType = recipe.dishType.map( item => titleCase(item) )
     })
-
     return recipeResults
 }
 
@@ -129,4 +147,9 @@ export function titleCase(string) {
     return string.slice(0,1).toUpperCase() + string.slice(1)
 }
 
-//export {titleCase, formatResults, parseResults, fetchJson, renderResults, search}
+export function renderNoResultState(){
+    document.getElementById('results-list').innerHTML = ""
+    document.getElementById('message-display').innerHTML = `
+        <p class='empty-state-message'> Unable to find what you're looking for. Please try another search.</p>
+    `
+}
